7359ff50ca481bd2eea664a9c0740280a8b0cec2
Dimitrios Lahanas <dimitrilahanas@gmail.com>
2026-02-10 21:03:54 +1100

    add(Home): added scaffold & appbar.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,6 +5,19 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        scrolledUnderElevation: 0,
+        title: Text(
+          "Dashboard",
+          style: TextStyle(
+            fontSize: 15,
+            color: Theme.of(context).colorScheme.primary,
+            fontWeight: FontWeight.w500,
+          )
+        ),
+      ),
+    );
   }
 }
