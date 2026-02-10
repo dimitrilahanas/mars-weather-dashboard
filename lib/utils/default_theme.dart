aa88ed3a1e0957bbae2a023f7253a69533e0eeff
Dimitrios Lahanas <dimitrilahanas@gmail.com>
2026-02-10 20:52:01 +1100

    add(DefaultTheme): added colorScheme to themes.

--- lib/utils/default_theme.dart
+++ lib/utils/default_theme.dart
@@ -1,6 +1,31 @@
 import 'package:flutter/material.dart';
 
 class DefaultTheme {
-  static ThemeData lightTheme = ThemeData();
-  static ThemeData darkTheme = ThemeData();
+  static ThemeData lightTheme = ThemeData(
+    colorScheme: ColorScheme(
+      brightness: Brightness.light,
+      primary: Color(0xFF0a0a0a),
+      onPrimary: Color(0xFF252525),
+      secondary: Colors.white,
+      onSecondary: Color(0xFFE7E7E7),
+      error: Color(0xFF2DD4BF),
+      onError: Colors.yellow,
+      surface: Color(0xFFf5f5f5),
+      onSurface: Colors.white,
+    ),
+  );
+
+  static ThemeData darkTheme = ThemeData(
+    colorScheme: ColorScheme(
+      brightness: Brightness.dark,
+      primary: Color(0xFFf0f0f0),
+      onPrimary: Color.fromARGB(255, 213, 213, 213),
+      secondary: Color(0xFF1a1a1a),
+      onSecondary: Color(0xFF1f1f1f),
+      error: Color(0xFF2DD4BF),
+      onError: Colors.yellow,
+      surface: Color(0xFF1f1f1f),
+      onSurface: Colors.black,
+    ),
+  );
 }
