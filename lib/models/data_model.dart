3f242f21e2f08da6d9abe329988df0feaa4d370f
Dimitrios Lahanas <dimitrilahanas@gmail.com>
2026-02-16 11:21:34 +1100

    add(DataModel): added data model.

---
+++ lib/models/data_model.dart
@@ -0,0 +1,11 @@
+class DataModel {
+  final String label;
+  final String data;
+  final String unit;
+
+  const DataModel({
+    required this.label,
+    required this.data,
+    required this.unit,
+  });
+}
